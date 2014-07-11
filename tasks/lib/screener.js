9e3563e63a5a4267422e7f088b6e63c800463194
dtothefp <dtothefp@gmail.com>
2014-07-11 18:28:52 -0400

    compare images with gm

--- tasks/lib/screener.js
+++ tasks/lib/screener.js
@@ -1,9 +1,15 @@
 'use strict';
 
+var http        = require('http');
 var fs          = require('fs');
+var fsRecur     = require('node-fs');
 var path        = require('path');
 var phantomjs   = require('phantomjs');
 var phantomPath = phantomjs.path;
+var resemblejs  = require('resemblejs');
+var HTTP        = require('q-io/http');
+var Q           = require('q');
+var gm          = require('gm');
 
 // used because fs.rmdirSync will not remove non-empty directories 
 var deleteFolderRecursive = function(path) {
@@ -31,6 +37,27 @@ var Screener = function (grunt, files, options, callback) {
   this.pictures = this.makePicPaths();
   this.screenNames = [];
   this.pictureCount = 0;
+  this.previousScreens = this.previousScreensGenerated();
+};
+
+Screener.prototype.previousScreensGenerated = function() {
+  //TODO: Handle scenarios for removed directories by user
+  if ( !fs.existsSync(this.options.indexPath) ) {
+    fsRecur.mkdirSync(this.options.indexPath + 'img/screens', '0777', true);
+    return false;
+  } 
+  else if ( !fs.existsSync(this.options.indexPath + 'img') ) {
+    fsRecur.mkdirSync(this.options.indexPath + 'img/screens', '0777', true);
+    return false;
+  }
+  else if (!fs.existsSync(this.options.indexPath + 'img/screens')) {
+    fs.mkdirSync(this.options.indexPath + 'img/screens');
+    return false;
+  }
+  else {
+    return true;
+  }
+  console.log("screens dir exists", fs.existsSync(this.options.indexPath + 'img/screens'));
 };
 
 // Make all paths for screenshots, concated with baseUrl and screenSizes, index.html removed 
@@ -90,55 +117,73 @@ Screener.prototype.takeScreenShots = function() {
       opts : opts
     }, function( err, result, code ) {
       this.pictureCount += 1;
-      if (this.pictureCount === this.pictures.length) {
-        console.log("handle images");
-        this.handleImages(err, result, code);
-      }
-    }.bind( this ) );
+      this.handleImages(err, result, code, picture);
+    }.bind(this) );
 
   }, this);
 };
 
-Screener.prototype.handleImages = function(err, result, code) {
-  // if screens have been previously taken
-  if ( fs.existsSync(this.options.indexPath + 'img/screens') ) {
-    this.pictures.forEach(function(picture) {
-      var pictureName = picture.name + '.png';
-      // do comparison with resemble JS here
-      var latestScreen = this.grunt.file.read(this.options.indexPath + 'img/tmp/' + pictureName);
-      var previousScreen = this.grunt.file.read(this.options.indexPath + 'img/screens/' + pictureName);
-      this.grunt.file.write(this.options.indexPath + 'img/screens/' + pictureName, latestScreen);
-      var latestScreen = fs.createReadStream(this.options.indexPath + 'img/tmp/' + pictureName);
-
-    }, this);
-  }
-  // if screens have not been previously taken
-  else {
-    // create screens directory
-    fs.mkdirSync(this.options.indexPath + 'img/screens');
-
-    // copy immediately from tmp to screens directory as these are the first shots taken for otherst to be diffed against
-    this.pictures.forEach(function(picture) {
-      var pictureName = picture.name + '.png';
-      
-      //var latestScreen = this.grunt.file.read(this.options.indexPath + 'img/tmp/' + pictureName);
-      //this.grunt.file.write(this.options.indexPath + 'img/screens/' + pictureName, latestScreen);
-
-      fs.readFile(this.options.indexPath + 'img/tmp/' + pictureName, function(err, original_data){
-          //TODO: handle errors
-          var base64Image = original_data.toString('base64');
-          var decodedImage = new Buffer(base64Image, 'base64');
-          console.log(this.options.indexPath + 'img/screens/' + pictureName);
-          fs.writeFile(this.options.indexPath + 'img/screens/' + pictureName, decodedImage, function(err) {
-            console.log("write file", err);
-          });
+Screener.prototype.readWriteDecodeImg = function (readFrom, writeTo) {
+  fs.readFile(readFrom, function(err, original_data){
+      //TODO: handle errors
+      var base64Image = original_data.toString('base64');
+      var decodedImage = new Buffer(base64Image, 'base64');
+      fs.writeFile(writeTo, decodedImage, function(err) {
+        if(!err) {
+          if(this.previousScreens) {
+            console.log('Overwriting previous screen: ' + writeTo + ' with: ' + readFrom );
+          } else {
+            console.log('First screens created: ' + writeTo);
+          }
+          if (this.pictureCount === this.pictures.length) {
+            //TODO: sometimes delete temp is firing twice
+            //console.log("delete temp");
+            //deleteFolderRecursive(this.options.indexPath + 'img/tmp');
+          }
+        }
+        else {
+          console.log("write file error", err);
+        }
       }.bind(this));
+  }.bind(this));
+};
 
-    }, this);
+Screener.prototype.handleImages = function(err, result, code, picture) {
+  var pictureName = picture.name + '.png';
+  var tempPath = this.options.indexPath + 'img/tmp/' + pictureName;
+  var screensPath = this.options.indexPath + 'img/screens/' + pictureName;
+
+  // if screens have been previously taken check if they are equal -- don't write from tmp to screens, if not equal -- write form tmp to screens 
+  if (this.previousScreens) {
+
+    gm.compare(
+      tempPath, 
+      screensPath, 
+      function (err, isEqual, equality, raw) {
+        //console.log('Previous Screen: ' + tempPath + ' and Latest Screen: ' + screensPath);
+        if(err) {
+          console.log(err);
+          //console.log('Previous Screen: ' + tempPath + ' and Latest Screen: ' + screensPath + 'could not be compared');
+        } 
+        // Images are equal, do nothing so they are not diffed on GitHub
+        else if (isEqual) {
+          console.log('Previous screen: ' + screensPath + ' is unchanged from current screen: ' + screensPath );
+          if (this.pictureCount === this.pictures.length) {
+            //TODO: sometimes delete temp is firing twice
+            //console.log("delete temp");
+            //deleteFolderRecursive(this.options.indexPath + 'img/tmp');
+          }
+        } 
+        else {
+          //TODO: Fix double logic here, try to find a way to read files with node and compare with gm or resemblejs
+          this.readWriteDecodeImg(tempPath, screensPath);
+        }
+    }.bind(this));
   }
-
-  // delete img directory if it already exists
-  deleteFolderRecursive(this.options.indexPath + 'img/tmp');
+  // if screens have not previously been taken write them immediately from tmp to screens
+  else {
+    this.readWriteDecodeImg(tempPath, screensPath);
+  }
 };
 
 // expose the Screener object
